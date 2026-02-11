6ec7b940ef236258cbc6cf1d2ce62a4045e5eca8
hemani-cloud <jhema.it@gmail.com>
2026-02-11 16:09:45 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import QuizList from "./components/QuizList"
-import QuizPage from "./components/QuizPage"
-import Result from "./components/Result"
+import Quiz from './components/Quiz'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<QuizList />} />
-        <Route path="/quiz/:id" element={<QuizPage />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Quiz />
+    </div>
   )
 }
 
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function Quiz() {
+  const [username, setUsername] = useState("");
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState({});
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/questions/")
+      .then(res => setQuestions(res.data));
+  }, []);
+
+  const handleOptionChange = (qId, isCorrect) => {
+    setAnswers({
+      ...answers,
+      [qId]: isCorrect
+    });
+  };
+
+  const submitQuiz = () => {
+    let score = 0;
+    Object.values(answers).forEach(val => {
+      if (val) score++;
+    });
+
+    axios.post("http://127.0.0.1:8000/api/submit-result/", {
+      username: username,
+      score: score,
+      total_questions: questions.length
+    })
+    .then(res => setResult(res.data));
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Quiz Application</h1>
+
+      {!result && (
+        <>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+
+          {questions.map((q, index) => (
+            <div key={q.id}>
+              <h3>{index + 1}. {q.question}</h3>
+              {q.options.map(opt => (
+                <label key={opt.id}>
+                  <input
+                    type="radio"
+                    name={`question-${q.id}`}
+                    onChange={() =>
+                      handleOptionChange(q.id, opt.is_correct)
+                    }
+                  />
+                  {opt.text}
+                  <br />
+                </label>
+              ))}
+            </div>
+          ))}
+
+          <button onClick={submitQuiz}>Submit Quiz</button>
+        </>
+      )}
+
+      {result && (
+        <div>
+          <h2>🎉 Result</h2>
+          <p><b>User:</b> {result.username}</p>
+          <p><b>Score:</b> {result.score} / {result.total_questions}</p>
+          <p><b>Date:</b> {new Date(result.submitted_at).toLocaleString()}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Quiz;
